e29b0b771d83184a823fe07be2336870be59e7c0
Biktot <164114980+Biktot@users.noreply.github.com>
2026-09-25 10:29:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-    applicationId = "moe.rukamori.archivetune"
+    applicationId = "com.netease.cloudmusic"
         minSdk = 26
         targetSdk = 37
         versionCode = 141
